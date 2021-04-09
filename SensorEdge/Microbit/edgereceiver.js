ca77cd40380221cefb8107ff2b4e10e60307be12
wzynus <e0191599@u.nus.edu>
2021-04-09 11:24:23 +0800

    adding the cloudserver, sensor edge, and gate edge connector

---
+++ SensorEdge/Microbit/edgereceiver.js
@@ -0,0 +1,35 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "int" && state == 0) {
+        serial.writeLine(radio.receivedPacket(RadioPacketProperty.SerialNumber) + " " + name + " " + value)
+        music.playTone(Note.C, music.beat())
+    } else if (name == "awake") {
+        if (state == 1) {
+            radio.sendValue("unarm", 1)
+        } else if (state == 0) {
+            radio.sendValue("arm", 0)
+        }
+    }
+})
+
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    let msg = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    let list = msg.split("$")
+    if (list[0] == "unarm") {
+        state = 1
+        radio.sendValue("unarm", 1)
+        music.playTone(Note.C, music.beat())
+    } else if (list[1] == "arm") {
+        state = 0
+        radio.sendValue("arm", 0)
+        music.playTone(Note.G, music.beat())
+    }
+})
+
+radio.setGroup(69)
+radio.setTransmitPower(7)
+radio.setFrequencyBand(7)
+radio.setTransmitSerialNumber(true)
+let state = 0
+basic.forever(function () {
+	
+})
---
+++ SensorEdge/Microbit/windownode.js
@@ -0,0 +1,40 @@
+let distance = 999
+let state = -1
+radio.setGroup(69)
+radio.setTransmitPower(7)
+radio.setFrequencyBand(7)
+radio.setTransmitSerialNumber(true)
+
+input.onButtonPressed(Button.AB, function () {
+    radio.sendValue("awake", 0);
+})
+
+
+radio.onReceivedValue(function (name: string, value: number) {
+    if (name == "arm") {
+        state = 0
+        music.playTone(Note.C5, music.beat())
+    } else if (name == "unarm") {
+        state = 1
+        music.playTone(Note.C, music.beat())
+    }
+    basic.showNumber(state)
+})
+
+basic.forever(function () {
+    distance = grove.measureInCentimeters(DigitalPin.P2)
+    if (distance <= 30) {
+        if (state == 0) {
+            basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . . # . .
+            . # . # .
+            `)
+            music.playTone(Note.G4,music.beat())
+            radio.sendValue("int", 0)
+            basic.pause(10000)
+        }
+    }
+})
